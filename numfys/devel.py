a79daad08b276607c7cbb02461e8def368248fbf
Andreas Krogen <andreas.krogen@gmail.com>
2016-04-25 18:16:54 +0200

    Change default settings to numfys.devel.

---
+++ numfys/devel.py
@@ -0,0 +1,29 @@
+from .base import *
+import os
+
+# SECURITY WARNING: Do NOT run in debug mode on the production server.
+DEBUG = True
+
+# SECURITY WARNING: You have to generate a new secret key when setting up the production server.
+# Keep it secret.
+SECRET_KEY = 'zek$#1$0@(i$t5(q*ne-r$s29#yhe9)!g$y9vc_-th4*0dxfi4'
+
+# Overwrite urls file for use in development
+ROOT_URLCONF = 'numfys.urls_devel'
+
+# Use a more lightweight database than MySQL when developing locally.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+STATIC_URL = '/s/'
+# Declare where "collectstatic" shall place static files
+# os.path.split(BASE_DIR)[0] is the parent directory of BASE_DIR
+#STATIC_ROOT = os.path.join(os.path.split(BASE_DIR)[0], 'numfys_static')
+
+# The directory in which user-uploaded files are stored.
+MEDIA_ROOT = os.path.join(os.path.split(BASE_DIR)[0], 'numfys_media')
+MEDIA_URL = '/media/'
--- numfys/wsgi.py
+++ numfys/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "numfys.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "numfys.devel")
 
 application = get_wsgi_application()
